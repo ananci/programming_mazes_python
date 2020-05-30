266652d5b0d813d53fc7715866e90b60a3cd1e38
Anna Eilering <nahkki@gmail.com>
2020-05-30 22:58:06 +0000

    Moving things around and setting up a setup script to make the mazes lib accessible from examples.

--- chapter_2/binary_tree.py
+++ chapter_2/binary_tree.py
@@ -1,2 +1,22 @@
-from grid import Grid
+#!/usr/bin/env python
+
+from .grid import Grid
+import random
+
+class BinaryTree(object):
+
+    def on(self, grid):
+        for cell in grid.each_cell:
+            neighbors = []
+            if cell.north:
+                neighbors.append(cell.north)
+            if cell.east:
+                neighbors.append(cell.east)
+
+            neighbor = random.Choice(neighbors)
+            cell.link(neighbor)
+
+
+binary_maze = Grid.grid(4, 4)
+BinaryTree().on(binary_maze)
 
--- setup.py
+++ setup.py
@@ -1,3 +1,49 @@
-from setuptools import setup
+#!/usr/bin/env python
 
-setup(name='mazes')
+"""Copyright 2020 Anna Eilering."""
+
+import os
+from setuptools import setup, find_namespace_packages
+import sys
+
+
+def get_long_description():
+    """
+    Get Long Description text for Mazes.
+    :return: 'Stuff'
+    :rtype: String
+    """
+    # TODO - Add better description here.
+    return "Stuff"
+
+
+def get_license():
+    """
+    Get License text for Mazes.
+    :return: 'Stuff'
+    :rtype: String
+    """
+    # TODO - Set up a license for this
+    return "license"
+
+setup(
+    name='mazes',
+    version='0.0.1',
+    description='Implementation of mazes code from Programming Mazes book',
+    author='Anna Eilering',
+    author_email='nahkki@gmail.com',
+    url='https://github.com/ananci/programming_mazes_python',
+    install_requires=[],
+    package_dir = {'': 'mazes'},
+    packages=find_namespace_packages('mazes'),
+    license=get_license(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Students',
+        'Natural Language :: English',
+        'License :: Freely Distributable',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Utilities'],
+    )
