6bbaaed3c3c9304a490915e9aa83d525bbc63db0
Anna Eilering <aeilering@google.com>
2020-07-09 17:37:08 -0500

    Storing unknown progress

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@
 from distutils.core import setup
 
 setup(
-    name='mazes',
-    version='0.0.3',
-    author='Anna Eilering',
-    author_email='nahkki@gmail.com',
-    url='https://github.com/ananci/programming_mazes_python',
-    license='LICENSE',
-    description='Implementation of mazes code from Programming Mazes book',
-    long_description=open('README.md').read(),
-    install_requires=['cairosvg'],
+  name='mazes',
+  version='0.0.3',
+  author='Anna Eilering',
+  author_email='nahkki@gmail.com',
+  url='https://github.com/ananci/programming_mazes_python',
+  license='LICENSE',
+  description='Implementation of mazes code from Programming Mazes book',
+  long_description=open('README.md').read(),
+  install_requires=['cairosvg'],
 )
